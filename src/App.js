e2477072b609fa39f21484808a9a9cc2c3906006
Srinivas <srinivas365@proton.me>
2024-04-11 13:13:11 +0530

    feat: implemented logout functionality

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import AnalyticsChart from "./components/AnalyticsChart";
 import Filters from "./components/Filters";
 
 import "./App.css";
+import { logout } from "./slices/authSlice";
 
 function App() {
     return (
         <div>
             <Filters />
             <AnalyticsChart />
+            <button
+                className="border-4 border-solid border-red-500 rounded-md p-1 mx-12"
+                onClick={() => {
+                    logout();
+                }}
+            >
+                Logout
+            </button>
         </div>
     );
 }
